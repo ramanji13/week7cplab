416db8e5f0d9cfd3e3589795a4bb5a904c2d595d
ramanji13 <venkataramanjireddy13@gmail.com>
2024-12-03 16:38:51 +0530

    Multiplication of matrices

--- mulmat.c
+++ mulmat.c
@@ -35,7 +35,7 @@ int main() {
        
     
     for(int i=0;i<m; i++) {
-        for(int j=0;j<n; j++) {
+        for(int j=0;j<b; j++) {
             printf("%d\t",A[i][j]);
         }
         printf("\n");
